7e9461c59f969950dc0c3b8c0c5e94507491c40f
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2020-12-09 17:32:31 -0500

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,20 @@
+const inquirer = require('inquirer')
+
 // array of questions for user
-const questions = [
+const questions = [ 
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write the description of your project"
+       
+    },
+   
 
 ];
 
@@ -9,7 +24,10 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then( function (answers) {
+        console.log(answers)
+    }) 
 }
 
 // function call to initialize program
